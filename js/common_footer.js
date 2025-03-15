2ca420cc3b3236120c5570f23540b81c909d21c5
White-Peach-Film <45752796+White-Peach-Film@users.noreply.github.com>
2025-03-15 14:31:36 +0800

    Delete js directory

--- js/common_footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const backToTop = document.getElementById("back-to-top");
-
-    backToTop.addEventListener("click", function (e) {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-});
